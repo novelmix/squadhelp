a4ff959b977c940150d5a4351678a4f7bbab8140
AndrewS <andreyspace12@gmail.com>
2023-04-02 18:50:06 +0300

    Fixed seeders

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -1,15 +1,15 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Banks', [
+    return queryInterface.bulkInsert('banks', [
       {
-        cardNumber: '4564654564564564',
+        card_number: '4564654564564564',
         name: 'SquadHelp',
         expiry: '11/22',
         cvc: '453',
         balance: 0,
       },
       {
-        cardNumber: '4111111111111111',
+        card_number: '4111111111111111',
         name: 'yriy',
         expiry: '09/23',
         cvc: '505',

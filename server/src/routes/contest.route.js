63e6ca82fe94dfc1211bab1c85cb5affb985570c
AndrewS <andreyspace12@gmail.com>
2023-04-04 15:57:34 +0300

    Created controller and route Offer

--- server/src/routes/contest.route.js
+++ server/src/routes/contest.route.js
@@ -5,19 +5,15 @@ const {
   getContestById,
   downloadFile,
   updateContest,
-  setNewOffer,
-  setOfferStatus,
   getCreativeContests,
 } = require('../controllers/Contest.controller');
 const {
   canGetContest,
   onlyForCreative,
-  canSendOffer,
-  onlyForCustomerWhoCreateContest,
   onlyForCustomer,
 } = require('../middlewares/basic.middleware');
 const { checkToken } = require('../middlewares/auth.middleware');
-const { updateContestFile, uploadLogoFiles } = require('../utils/fileUpload');
+const { updateContestFile } = require('../utils/fileUpload');
 const contest = Router();
 
 contest.post('/dataForContest', checkToken, dataForContest);
@@ -26,18 +22,5 @@ contest.get('/getContestById', checkToken, canGetContest, getContestById);
 contest.post('/getCreativeContests', checkToken, onlyForCreative, getCreativeContests);
 contest.get('/downloadFile/:fileName', checkToken, downloadFile);
 contest.post('/updateContest', checkToken, updateContestFile, updateContest);
-contest.post(
-  '/setNewOffer',
-  checkToken,
-  uploadLogoFiles,
-  canSendOffer,
-  setNewOffer,
-);
-contest.post(
-  '/setOfferStatus',
-  checkToken,
-  onlyForCustomerWhoCreateContest,
-  setOfferStatus,
-);
 
 module.exports = contest;

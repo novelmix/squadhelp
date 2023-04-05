c80b61b70b22750374980db043177e04b9adb0f1
AndrewS <andreyspace12@gmail.com>
2023-04-05 10:43:16 +0300

    Fixed multer

---
+++ server/src/config/multer.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const { DEV_FILES_PATH } = require('../constants');
+const env = process.env.NODE_ENV || 'development';
+const devFilePath = path.resolve(DEV_FILES_PATH, 'images');
+
+const filePath = env === 'production' ? '/var/www/html/images/' : devFilePath;
+
+if (!fs.existsSync(filePath)) {
+  fs.mkdirSync(filePath, {
+    recursive: true,
+  });
+}
+
+const storageContestFiles = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, filePath);
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const multerInstance = multer({ storage: storageContestFiles });
+
+module.exports.uploadAvatar = multerInstance.single('file');
+module.exports.updateContestFile = multerInstance.single('file');
+module.exports.uploadContestFiles = multerInstance.array('files', 3);
+module.exports.uploadLogoFiles = multerInstance.single('offerData');

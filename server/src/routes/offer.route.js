2298f5b8dd7229c6d6e318adea70004892d96c0e
AndrewS <andreyspace12@gmail.com>
2023-04-24 13:16:25 +0300

    Added routes

--- server/src/routes/offer.route.js
+++ server/src/routes/offer.route.js
@@ -1,14 +1,35 @@
 const { Router } = require('express');
-const { setNewOffer, setOfferStatus } = require('../controllers/Offer.controller')
+const {
+  setNewOffer,
+  setOfferStatus,
+  getOffersForModerator,
+  updateOfferForModerator,
+} = require('../controllers/Offer.controller');
 const { checkToken } = require('../middlewares/auth.middleware');
-const { uploadLogoFiles } = require('../config/multer')
-const { canSendOffer, onlyForCustomerWhoCreateContest } = require('../middlewares/basic.middleware')
+const { uploadLogoFiles } = require('../config/multer');
+const {
+  canSendOffer,
+  onlyForCustomerWhoCreateContest,
+  onlyForModerator,
+} = require('../middlewares/basic.middleware');
+const pagination = require('../middlewares/pagination.middleware');
 
 const offer = Router();
 
+offer.get('/', checkToken, onlyForModerator, pagination, getOffersForModerator);
+offer.patch('/:offerId', checkToken, onlyForModerator, updateOfferForModerator);
 offer.post(
-  '/setNewOffer', checkToken, uploadLogoFiles, canSendOffer, setNewOffer); 
-  offer.post(
-  '/setOfferStatus', checkToken, onlyForCustomerWhoCreateContest, setOfferStatus);
+  '/setNewOffer',
+  checkToken,
+  uploadLogoFiles,
+  canSendOffer,
+  setNewOffer
+);
+offer.post(
+  '/setOfferStatus',
+  checkToken,
+  onlyForCustomerWhoCreateContest,
+  setOfferStatus
+);
 
 module.exports = offer;
